0a2cca0f33ff8040b199f4611356d8228646f932
Dat Nguyen <dnguyenk2@gmail.com>
2022-03-15 11:09:36 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(array, name){
+
+    var arr = [];
+    
+    for (let i = 0; i < array.length; i++){
+        arr.push("Thank you, " + array[i] + ", for the wonderful " + name + " gift!");
+    };
+
+    return arr;
+}
+;
+
+function countDown(number){
+    for (number; number >= 0; number--){
+        console.log(number);
+    }
+};
+
+
